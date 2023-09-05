c4c2a3237cfde2d822d99b3b172c4457d00f460a
Ryan Bush <cold@coldlogical.com>
2023-09-05 20:12:07 +0900

    FIX: Formatting changes

--- Sources/BillboardEntity/BillboardEntity.swift
+++ Sources/BillboardEntity/BillboardEntity.swift
@@ -3,41 +3,41 @@ import RealityKit
 import Combine
 
 public class BillboardEntity: Entity {
-        var arView: ARView {
-                didSet {
-                        subscribeToUpdates(of: arView.scene)
-                }
+    var arView: ARView {
+        didSet {
+            subscribeToUpdates(of: arView.scene)
         }
-        private var sceneObserver: Cancellable!
-
-        // MARK: - Initializers
-        required init() {
-                fatalError("\(#function): BillboardEntity requires an ARView. Use init(orientatedTo:)")
+    }
+    private var sceneObserver: Cancellable!
+    
+    // MARK: - Initializers
+    required init() {
+        fatalError("\(#function): BillboardEntity requires an ARView. Use init(orientatedTo:)")
+    }
+    
+    public init(orientatedTo arView: ARView) {
+        self.arView = arView
+        super.init()
+        
+        subscribeToUpdates(of: arView.scene)
+    }
+    
+    
+    // MARK: - Operational
+    fileprivate func subscribeToUpdates(of scene: Scene) {
+        sceneObserver = scene.subscribe(to: SceneEvents.Update.self) {
+            [unowned self] _ in
+            self.orientateToCamera()
         }
-
-        public init(orientatedTo arView: ARView) {
-                self.arView = arView
-                super.init()
-
-                subscribeToUpdates(of: arView.scene)
+    }
+    
+    fileprivate func orientateToCamera() {
+        guard parent != nil
+        else {
+            return
         }
-
-
-        // MARK: - Operational
-        fileprivate func subscribeToUpdates(of scene: Scene) {
-                sceneObserver = scene.subscribe(to: SceneEvents.Update.self) {
-                        [unowned self] _ in
-                        self.orientateToCamera()
-                }
-        }
-
-        fileprivate func orientateToCamera() {
-                guard parent != nil
-                else {
-                        return
-                }
-
-                let camTransform = arView.cameraTransform
-                setOrientation(camTransform.rotation, relativeTo: nil)
-        }
+        
+        let camTransform = arView.cameraTransform
+        setOrientation(camTransform.rotation, relativeTo: nil)
+    }
 }
